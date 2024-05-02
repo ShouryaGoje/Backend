de0469886e8380273524d94953deea8ca4233631
ShouryaGoje <shouryagoje@gmail.com>
2024-05-02 10:58:32 +0530

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-echo "Building project packages..."
-python3 m pip install -r requirements.txt
-echo "Migrating Database..."
-python3 manage.py makemigrations-noinput
-python3 manage.py migrate --noinput
-echo "Collecting static files..."
-python3 manage.py collectstatic-noinput
+# build_files.sh
+
+pip install -r requirements.txt
+python3.10 manage.py collectstatic --no-input --clear
+python3.10 manage.py makemigrations --no-input --clear
+python3.10 manage.py migrate --no-input --clear
